68d3866dc195983709904a570da7672e37caf129
Caliendo Gianluca <gianlucacaliendo@libero.it>
2019-09-16 20:38:17 +0200

    Initial Commit

---
+++ Beer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Beer
+//
+//  Created by Gianluca Caliendo on 16/09/2019.
+//  Copyright © 2019 Gianluca Caliendo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
